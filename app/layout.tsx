67e70662c4358c3d6192f42504a5442822af3950
tobiasraisanen-blip <tobias.raisanen@hotmail.com>
2026-05-18 09:34:37 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="sv" translate="no" className="notranslate">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
